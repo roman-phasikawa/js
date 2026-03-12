d943a03c7e2bc7df3692a7bfa75fb4ad4af88dd7
Roman Phasikawa <romanphasikawa@gmail.com>
2026-03-13 00:26:17 +0545

    Functions remaing

--- 06-function/function.js
+++ 06-function/function.js
@@ -54,3 +54,69 @@ function findMax(numbers) {
   return max;
 }
 console.log(findMax(numMax));
+
+// Create a function named reverseString that takes a string as input
+// and returns the string reversed. For instance, "hello" should become
+// "olleh"
+const reverseString = (str) => {
+  let newString = "";
+  for (let i = str.length-1; i >= 0; i--) {
+    newString += str[i];
+  }
+  return newString;
+};
+console.log(reverseString("hello"));
+
+// Write a function called createUsername that takes firstName and
+// lastName as parameters. If lastName is not provided, it should default
+// to "User". Return username as "firstname_lastname" in lowercase.
+const createUsername=(firstName,lastName="User")=>{
+  return `${firstName}_${lastName}`.toLowerCase();
+};
+console.log(createUsername("Ram"));
+
+// Create a function called applyOperation that takes two numbers and
+// a callback function as parameters. The callback should perform an
+// operation on the two numbers
+const addNum=(a,b)=>{
+  return a+b;
+}
+const applyOperation = (n1, n2, callback) => {
+  return callback(n1,n2);
+};
+console.log(applyOperation(2, 3,addNum));
+
+// Write a function named filterAdults that takes an array of ages and
+// returns a new array containing only ages 18 or above using the
+// filter() method
+const filterAdults = (arr) => {
+  return arr.filter(age=>age>=18);  
+};
+const arrAge = [12, 18, 16, 21, 30];
+console.log(filterAdults(arrAge));
+
+//  Create a function called calculateGrade that takes a score (0-100) 
+// and returns a letter grade: A (90-100), B (80-89), C (70-79), D (60-69), F 
+// (below 60)
+const calculateGrade = (grade)=>{
+  if(grade>=90)
+    return "A";
+  else if(grade<90&&grade>=80)
+    return "B";
+  else if(grade<80&&grade>=70)
+    return "C";
+  else if(grade<70&&grade>=60)
+    return "D";
+  else
+    return "F";
+}
+console.log(calculateGrade("70"));
+
+// Write a function isPalindrome that checks if a string is a palindrome 
+// (reads the same forwards and backwards), ignoring spaces and cases
+const isPalindrome = (str) => {
+  let reversedStr = str.split('').reverse().join('');
+  return str === reversedStr;
+}
+console.log(isPalindrome("ram")); 
+console.log(isPalindrome("mom")); 
